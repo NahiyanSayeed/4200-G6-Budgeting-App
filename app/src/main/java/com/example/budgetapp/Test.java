67e4a1bd353797a4d1313feffa59ee3b9b32572c
Spondon <spondon2511@gmail.com>
2025-03-24 01:35:59 -0400

    Added as a playbox

---
+++ app/src/main/java/com/example/budgetapp/Test.java
@@ -0,0 +1,25 @@
+package com.example.budgetapp;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Test extends AppCompatActivity {
+
+    TextView contentText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
+}
+
